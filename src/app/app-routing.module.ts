eb96764dbc9a64d6e91645696224d4dbef29368b
Martin <martindelacruzboza@gmail.com>
2021-01-27 11:32:52 -0500

    new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './pages/content/content.component';
+import { TestComponent } from './pages/test/test.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/' }
+  {path:'content',component:ContentComponent},
+  {path:'test',component:TestComponent},
+  { path: '**', pathMatch: 'full', redirectTo: '/content' }
 ];
 
 @NgModule({
